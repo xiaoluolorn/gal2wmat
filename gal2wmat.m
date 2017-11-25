336950b1d919e9206c602febbc5fa30bc311bb92
xiaoluolorn <742931204@qq.com>
2017-11-25 23:58:32 +0800

    update

--- gal2wmat.m
+++ gal2wmat.m
@@ -1,31 +1,43 @@
 function [wmat] = gal2wmat(file)
-% 'file' is the name of the .gal document.
-% To use this function, you need to reshape the .gal document:
-% At first, delete the first line of the .gal document;
-% then, add a 0 to the next line if the district have no neighbors.
-% eg: Invert this kind of format
-%   0 34 map DZM   %
-%   8 2            %
-%   7 5            %
-%   30 0           %
-%                  %
-%   to the format like this:
-%   8 2            %
-%   7 5            %
-%   30 0           %
-%   0              %
-aa = textread(file);
-wmatt = size(aa);
+% To get the row number of .gal document.
+fidd = fopen(file,'r');
+row=0;
+while ~feof(fidd) 
+    [~]=fgets(fidd); 
+    row=row+1; 
+end
+fclose(fidd);
+% Read .gal document by rows.
+fid = fopen(file,'r');
+ww=zeros(row);
+for i=1:row
+t=fgetl(fid);
+rownum=str2num(t);
+s=row-max(size(rownum));
+ss=zeros(1,s);
+tttt= [rownum ss];
+ww(i,:)=ww(i,:) + tttt;
+end
+fclose(fid);
+for ii=1:row
+    if sum(ww(:,ii)) > 0
+        m=ii;
+    end
+end
+
+www=ww(2:row,1:m);
+ % Convert sparse matrix to matrix.
+wmatt = size(www);
 nn=max(wmatt)/2;
 wmattt=zeros(nn,nn);
    for k=1:nn
     l=2*k-1;
     m=2*k;
-    n=aa(l,2);
+    n=www(l,2);
      if n > 0
        for ii = 1:n 
-        jj = aa(m,ii);
-        kk = aa(l,1);
+        jj = www(m,ii);
+        kk = www(l,1);
         wmattt(kk,jj) = 1;
         wmattt(jj,kk) = 1;
        end 
@@ -33,4 +45,16 @@ wmattt=zeros(nn,nn);
    end
    wmat = wmattt;
 end
+
+
+
+
+
+
+
+
+
+
+
+
 
